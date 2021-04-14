59452d1d2d3a8483bc799825ad4f18c1f424c9b3
kfriedman <kfriedman@H10897.ad.hugeinc.com>
2021-04-13 21:51:47 -0400

    feat(netlify config): set up env variables

--- functions/sendEnvVariables.js
+++ functions/sendEnvVariables.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+const { CLIENT_ID } = process.env;
+
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ CLIENT_ID: "client_id_test" }),
+    body: JSON.stringify({ CLIENT_ID }),
   };
 };
