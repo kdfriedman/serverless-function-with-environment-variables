5b495d6273d7d9f97542890550746fd16a21be10
kfriedman <kfriedman@H10897.ad.hugeinc.com>
2021-04-15 16:18:14 -0400

    config(env variables): update import environment variables from env file

--- functions/sendEnvVariables.js
+++ functions/sendEnvVariables.js
@@ -1,9 +1,62 @@
+//initialize dotenv library for env variable usage within dev
 require("dotenv").config();
-const { CLIENT_ID } = process.env;
+// firebase config object
+const {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} = process.env;
 
+// function handler which is netlify uses to pass in two arguments - event + context
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ CLIENT_ID }),
-  };
+  // initial request made automatically by browser
+  switch (event.httpMethod) {
+    case "OPTIONS": {
+      console.log(`inside ${event.httpMethod} case`);
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": process.env.ORIGIN,
+          "Access-Control-Allow-Headers":
+            "Origin, X-Requested-With, Content-Type, Accept",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Methods": "OPTIONS, POST",
+          "Access-Control-Allow-Credentials": "true",
+        },
+      };
+    }
+    // get env data to send back to client
+    case "POST": {
+      console.log(`inside ${event.httpMethod} case`);
+      return {
+        statusCode: 200,
+        // only allow origin, and OPTIONS + POST HTTP methods
+        headers: {
+          "Access-Control-Allow-Origin": process.env.ORIGIN,
+          "Access-Control-Allow-Headers":
+            "Origin, X-Requested-With, Content-Type, Accept",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Methods": "OPTIONS, POST",
+          "Access-Control-Allow-Credentials": "true",
+        },
+        // send back env variable object
+        // map identical values to netlify env variable setup in dashboard for PROD
+        body: JSON.stringify({
+          API_KEY,
+          AUTH_DOMAIN,
+          PROJECT_ID,
+          STORAGE_BUCKET,
+          MESSAGING_SENDER_ID,
+          APP_ID,
+        }),
+      };
+    }
+    default: {
+      console.log(`inside ${event.httpMethod} case`);
+      return { statusCode: 405, body: "Method Not Allowed" };
+    }
+  }
 };
